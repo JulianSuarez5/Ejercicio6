281e5f52654657ad13431042a83ded03a057c96b
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:32:13 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ParImpar
+{
+    static void Main()
+    {
+        int numero;
+
+        Console.Write("Ingrese un número: ");
+        numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número es par");
+        }
+        else
+        {
+            Console.WriteLine("El número es impar");
+        }
+
+        Console.ReadKey();
+    }
+}
+
